d24f97403eeea0fe5e7dafbad1896f53f3a3c332
Hayk Papoyan <imhayk@gmail.com>
2026-06-17 19:03:12 +0200

    Added player marker

---
+++ Assets/SledSurfers/Scripts/Player/PlayerMarker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace SledSurfers.Scripts.Player
+{
+    public class PlayerMarker : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(transform.position, new Vector3(2f, 1f, 2f));
+        }
+    }
+}
