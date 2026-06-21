f909353f84b0ec59dd4c24eb7c814d2ee7a9de2e
Hayk Papoyan <imhayk@gmail.com>
2026-06-21 23:42:48 +0200

    fix avoiding warning

--- Assets/SledSurfers/Scripts/Player/PlayerLaunchController.cs
+++ Assets/SledSurfers/Scripts/Player/PlayerLaunchController.cs
@@ -24,9 +24,12 @@ namespace SledSurfers.Scripts.Player
         
         public void ResetPlayer(Transform spawnPoint)
         {
-            _rigidbody.isKinematic = true;
-            _rigidbody.velocity = Vector3.zero;
-            _rigidbody.angularVelocity = Vector3.zero;
+            if (!_rigidbody.isKinematic)
+            {
+                _rigidbody.velocity = Vector3.zero;
+                _rigidbody.angularVelocity = Vector3.zero;    
+                _rigidbody.isKinematic = true;
+            }
 
             _rigidbody.position = spawnPoint.position;
             _rigidbody.rotation = spawnPoint.rotation;
